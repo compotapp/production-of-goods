73bf7341c897dc9f8083f5ad0b68d5bd65e9d669
compotapp <alexandrpotapov90@gmail.com>
2026-02-21 14:02:42 +0300

    POG/main: first commit

---
+++ src/test/java/com/pot/app/productionofgoods/ProductionOfGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pot.app.productionofgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionOfGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
