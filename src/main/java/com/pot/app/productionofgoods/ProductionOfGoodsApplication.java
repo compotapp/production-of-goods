bb015b146e251415852785106f49199656eff87c
compotapp <alexandrpotapov90@gmail.com>
2026-02-21 14:02:42 +0300

    POG/main: first commit

---
+++ src/main/java/com/pot/app/productionofgoods/ProductionOfGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.pot.app.productionofgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionOfGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionOfGoodsApplication.class, args);
+    }
+
+}
